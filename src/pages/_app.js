74ed2a258e2fb3b316038e5cc278926c54d2f348
Timothy Mai <timothy.mai@pacificlife.com>
2021-03-18 11:27:13 -0700

    add styled-components

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,28 @@
-import "../../styles/globals.css";
+/* eslint-disable react/jsx-props-no-spreading */
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+const theme = {
+  colors: {
+    primary: "#0070f3",
+  },
+};
 
 // eslint-disable-next-line react/prop-types
-function MyApp({ Component, pageProps }) {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
-
-export default MyApp;
