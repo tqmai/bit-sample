dfea8f1fbdeaa3b230cb94ac36eacba44828825c
Timothy Mai <timothy.mai@pacificlife.com>
2021-03-18 11:17:29 -0700

    create src folder, reorganize file structure

---
+++ src/components/Sample.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Sample = () => <div>Sample</div>;
+
+export default Sample;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import "../styles/globals.css";
+import "../../styles/globals.css";
 
 // eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
